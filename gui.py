87aa3110efb348c28c49157d7380676cd8ba9c93
omprakash <omprakaxh@gmail.com>
2024-02-06 18:57:43 +0530

    impleted delete button and exit button

--- gui.py
+++ gui.py
@@ -7,10 +7,14 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(function.get_todos(),key="todos",
                       enable_events=True, size= [40, 10])
 edit_button = sg.Button("Edit")
-
+delete_button = sg.Button("Delete")
+exit_button = sg.Button("Exit")
 
 window = sg.Window('My To-Do App',
-                   layout=[[label], [text_box,add_button], [list_box,edit_button]] ,
+                   layout=[[label],
+                           [text_box,add_button],
+                           [list_box,edit_button,delete_button],
+                           [exit_button]] ,
                    font=("Helvetica",20))
 
 while True:
@@ -32,6 +36,16 @@ while True:
             todos[index] = new_todo
             function.write_todos(todos)
             window["todos"].update(values=todos)
+        case "Delete":
+            todo_to_delete = values["todos"][0]
+            todos = function.get_todos()
+            todos.remove(todo_to_delete)
+            function.write_todos(todos)
+            window["todos"].update(values=todos)
+            window["todo"].update(value="")
+        case "Exit":
+            break
+
         case "todos":
             window["todo"].update(value=values['todos'][0])
 
